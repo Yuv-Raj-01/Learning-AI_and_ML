4e53cfa012e9e347c71ceff7313949df0c53046e
Yuvraj Yadav <yadavy@roehampton.ac.uk>
2026-07-28 21:28:19 +0100

    python1

---
+++ python/01/code.py
@@ -0,0 +1,9 @@
+print("Hello World")
+
+name = "Ram"
+print("my name is" + name)
+print("my name is",name)
+
+name = input("Please, Enter your name: ")
+age = int(input("Please, Enter your age: "))
+
---
+++ python/02/conditional_statement.py
@@ -0,0 +1,8 @@
+# conditional Statement
+
+age = int(input("Please, Enter your age: "))
+if (age > 18):
+    print("you can vote")
+    print("you can apply for drive")
+elif(age<18):
+    print("Hi, kid")
